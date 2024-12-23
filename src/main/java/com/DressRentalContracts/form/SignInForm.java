1bdc5facba56728cda627a6ae4091d97a7faa8b4
anvdmal <alekseeva.ann04@mail.ru>
2024-12-23 11:01:29 +0300

    add @Email validation

--- src/main/java/com/DressRentalContracts/form/SignInForm.java
+++ src/main/java/com/DressRentalContracts/form/SignInForm.java
@@ -1,10 +1,12 @@
 package com.DressRentalContracts.form;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 public record SignInForm(
         @NotEmpty(message = "Email обязателен")
+        @Email(message = "Некорректный формат email")
         String email,
         @NotEmpty(message = "Пароль обязателен")
         @Length(min = 8, message = "Пароль должен состоять минимум из восьми символов")
--- src/main/java/com/DressRentalContracts/form/SignUpForm.java
+++ src/main/java/com/DressRentalContracts/form/SignUpForm.java
@@ -1,5 +1,6 @@
 package com.DressRentalContracts.form;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
@@ -8,6 +9,7 @@ public record SignUpForm(
         @Length(min = 2, max = 40, message = "Имя должно содержать минимум два символа")
         String name,
         @NotEmpty(message = "Email обязателен")
+        @Email(message = "Некорректный формат email")
         String email,
         @NotEmpty(message = "Пароль обязателен")
         @Length(min = 8, message = "Пароль должен состоять минимум из восьми символов")
